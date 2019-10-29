a4b906da00196758f093ede945e072a12ba43fc2
Alvaro Zumaran <alvarozumaran@hotmail.com>
2019-10-29 08:34:21 -0500

    proyecto actualizado 2

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,4 +1,4 @@
-#Entrada
+# Entrada
 import os
 os.system("cls") 
 
@@ -7,7 +7,7 @@ ponderado = []
 nivel_ingles = []
 edad = []
 datos = [nombres, edad,ponderado, nivel_ingles ]
-
+print("***INGRESAR POSTULANTES***")
 while True:
 
     n = input("Ingresa Nombre: ")
@@ -29,11 +29,35 @@ while True:
     if pregunta == "N" or pregunta == "n":
         break
 
-#Proceso
+# Proceso
+nom_apto=[]
+edad_ap=[]
+ponde_ap=[]
+nive_ap=[]
+aceptados=[nom_apto,edad_ap,ponde_ap,nive_ap]
+for i in range(len(nombres)):
+    if edad[i] >= 18:
+        # aceptados=[edad]
+        if ponderado[i] >= 16:
+            # aceptado=[edad,ponderado]
+            if nivel_ingles[i] == "Intermedio" or  nivel_ingles[i] =="intermedio"or  nivel_ingles[i] =="Avanzado"or  nivel_ingles[i] =="avanzado":
+                # aceptados.append(nombres[i], edad[i] ,ponderado[i],nivel_ingles[i])
+                a = nombres[i]
+                nom_apto.append(a)
+                b = edad[i]
+                edad_ap.append(b)
+                c = ponderado[i]
+                ponde_ap.append(c)
+                d = nivel_ingles[i]
+                nive_ap.append(d)
+        
+# Postulantes que obtienen la beca
 
-ponderado_max= max(ponderado)
-ponderadoaceptado=[ponderado_max]
-w=ponderado.index(ponderado_max)
 
+# Salida
+print("***Postulantes Aptos***")
 
-#Salida
+for i in range(len(nom_apto)):
+    print(
+        i+1,". Nombre:",aceptados[0][i],". Edad: ", aceptados[1][i],". Ponderado: ",aceptados[2][i],". Nivel de ingles: ",aceptados[3][i]
+    )
